ef16d951691357894fe0e83cfc1de1d0567d532c
hanwootech <89382407+hanwootech@users.noreply.github.com>
2023-05-31 23:23:53 +0900

    Add files via upload

---
+++ Koread/Koread-back-end/config/databaseConfig.js
@@ -0,0 +1,12 @@
+const databaseConfig = {
+    host : '3.39.132.224' ,
+    port : 3306,
+    user : 'mk' ,
+    password : 'Panda_1247' ,
+    database : 'book_db',
+    waitForConnections : true ,
+    connectionLimit : 10,
+    queueLimit : 0
+}
+
+module.exports = databaseConfig;
